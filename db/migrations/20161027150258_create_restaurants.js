fa392515c4391704426b207b4420657d7a7134ae
Mario Viens <marviens@gmail.com>
2016-10-27 15:33:58 -0700

    gave content to the restaurants table

--- db/migrations/20161027150258_create_restaurants.js
+++ db/migrations/20161027150258_create_restaurants.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable('restaurants', (table) => {
+    table.inc
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable('restaurant');
 };
