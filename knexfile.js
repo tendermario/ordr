f01b528750de113c8f6d3ac570730d0602e274a2
radtrav <t.w.young.ubc@gmail.com>
2016-10-27 21:07:28 +0000

    Added customer table

---
+++ knexfile.js
@@ -0,0 +1,48 @@
+// Update with your config settings.
+const settings = require("./settings"); // settings.json
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      filename: 'create_milestones'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'pg',
+    connection: {
+      database: settings.database,
+      user:     settings.user,
+      password: settings.password,
+      host:     settings.hostname,
+      ssl:      settings.ssl,
+      port:     settings.port
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+      directory: './db/migrations'
+    }
+  }
+
+};
