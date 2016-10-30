cc6f7fb52a947ceae8cbd4e44f7e441c57b2e770
radtrav <t.w.young.ubc@gmail.com>
2016-10-30 03:07:47 +0000

    Implement ajax post from restaurant.js to /restaurant end point, data contains the order_id of clicked chit

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,39 +4,39 @@ $(function() {
     $('.menu-item').on('click', function(event) {
         toggleFromCart($(this).find('div'));
     });
+    //-----------------------RESTAURANT------------------
     var ordersCompleted = [];
     $(".remove_chit").on('click', function(event) {
-        if ($(this).text() === "Queued: click to finish") {
-            $(this).text("Finished: click to queue")
-        } else {
-            $(this).text("Queued: click to finish");
-        }
-        //find what the order number of the chit is
-        let order_id = $(this).closest("header").find("#order_id").html()
-            // if the order is already in the array remove it
+
+        $(this).closest('.order_chit').addClass('finished');
+        let order_id = $(this).closest('.order_chit').data('order_id');
+        // if the order is already in the array remove it
         let orderInArray = ordersCompleted.indexOf(order_id);
         if (orderInArray !== -1) {
             ordersCompleted.splice(orderInArray, 1);
         } else {
             ordersCompleted.push(order_id);
         }
-
-
-         $(this).closest('.order_chit').data('id')
-
+        console.log('order_id',order_id);
 
         $.ajax({
-            url: '/restaurants',
-            type: 'post',
-            data: $(this).closest('.order_chit').data('id'),
-            dataType: 'text',
-        }).done(function(data) {
-            console.log('ajax post done');
-        }).fail(function(data) {
-            console.log('ajax post failed');
-        }).always(function(data) {
-            console.log('ajax post always');
+            url: "/restaurants",
+            type: "POST",
+            dataType: "json",
+            data: {order_id: order_id},
+            timeout: 5000,
+            complete: function() {
+                console.log('process complete');
+            },
+            success: function(data) {
+                console.log(data);
+                console.log('process sucess');
+            },
+            error: function() {
+                console.log('process error');
+            },
         });
+
+         $(this).remove();
     });
 });
-
--- routes/restaurants.js
+++ routes/restaurants.js
@@ -1,53 +1,35 @@
 "use strict";
 const express = require('express');
 const router = express.Router();
+
+
+
 // when accessing /restaurants, use these paths
 // e.g. my.com/restaurants/new => "/new"
 module.exports = (db) => {
+    router.get("/", (req, res) => {
+        // hard-coding restaurant as 1:
+        let arrayOfOrders = db.getOrders(1).then((result) => {
+            res.render("../views/restaurants/index", {
+                result
+            });
+        });
+    });
 
-        // let sampleOrderData = {
-        //   dbData: [{
-        //     id: 29,
-        //     customer_name: "Hungry Afdane",
-        //     order_date: "some date",
-        //     completed: false,
-        //     dishes: {
-        //         bagel: 1,
-        //         linguini: 1,
-        //         duck: 1,
-        //         pork: 2
-        //     },
-        //   },
-        //   {
-        //     id: 30,
-        //     customer_name: "TRavis",
-        //     order_date: "some date",
-        //     completed: false,
-        //     dishes: {
-        //         bagel: 1,
-        //         linguini: 10,
-        //         duck: 10,
-        //         pork: 2
-        //     },
-        //   }
+    router.post("/", function(req, res) {
+        console.log(req.body);
 
-        //   ]
-        // };
-        // res.render("../views/restaurants/index", sampleOrderData);
-  // when
-  router.get("/", (req, res) => {
-    // hard-coding restaurant as 1:
-    let arrayOfOrders = db.getOrders(1);
-    let renderedData = { arrayOfOrders }
-    res.render("../views/restaurants/index", renderedData);
-  });
+        res.contentType('json');
+        res.send({some: JSON.stringify({response: 'json'})});
+    });
 
-  router.get("/:id", (req, res) => {
-    let restaurant_id = req.params.id;
-    let arrayOfOrders = db.getOrders(restaurant_id);
-    let renderedData = { arrayOfOrders }
-    // passing order object to view
-    res.render("../view/restaurants/index", renderedData);
-  });
+    router.get("/:id", (req, res) => {
+        // hard-coding restaurant as 1:
+        let arrayOfOrders = db.getOrders(req.params.id).then((result) => {
+            res.render("../views/restaurants/index", {
+                result
+            });
+        });
+    });
     return router;
 }
